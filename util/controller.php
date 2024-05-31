8ffeca3dd3646a3d724a610ebf75585fd30c1920
nepikn <31944706+nepikn@users.noreply.github.com>
2024-06-01 01:28:23 +0800

    feat: forbid no ssid req except get

--- util/controller.php
+++ util/controller.php
@@ -8,6 +8,14 @@ class Controller {
     // $id = @$req['queries']['id'] ?? $this->id;
     // $req_prop = @$req['args']['prop'];
     // @['id' => $id, 'prop' => $prop] = $req['args'];
+    if (
+      in_array($method, ['POST', 'PUT', 'DELETE'])
+      && empty($_COOKIE['PHPSESSID'])
+    ) {
+      respond('no session id', 400);
+      return;
+    }
+
     $this->req = $req;
     return $this->$method();
   }
